6d508452286bd1483b75d854f5482c0acd2515be
Jacob Marthaller <marthaller.jr@gmail.com>
2021-04-20 09:49:23 -0700

    complete js workbench problem

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
-const url = "https://eloquentjavascript.net/author";
-const types = ["text/plain",
-               "text/html",
-               "application/json",
-               "application/rainbows+unicorns"];
+// const url = "https://eloquentjavascript.net/author";
+// const types = ["text/plain",
+//                "text/html",
+//                "application/json",
+//                "application/rainbows+unicorns"];
 
-async function showTypes() {
-  for (let type of types) {
-    let resp = await fetch(url, {headers: {accept: type}});
-    console.log(`${type}: ${await resp.text()}\n`);
-  }
-}
+// async function showTypes() {
+//   for (let type of types) {
+//     let resp = await fetch(url, {headers: {accept: type}});
+//     console.log(`${type}: ${await resp.text()}\n`);
+//   }
+// }
 
-showTypes();
+// showTypes();
+
+
+document.querySelector("#button").addEventListener("click", () => {
+    let code = document.querySelector("#code").value;
+    let outputNode = document.querySelector("#output");
+    try {
+      let result = Function(code)();
+      outputNode.innerText = String(result);
+    } catch (e) {
+      outputNode.innerText = "Error: " + e;
+    }
+  });
